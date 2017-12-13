c20c2c58aa841c68b0c858e285dd581ceb998911
Rachel Miller <raeanne1991@gmail.com>
2017-12-13 02:45:13 +0000

    added more structure to cli.rb and created bars.rb for future scraping.

---
+++ manhattan_bar_directory/lib/manhattan_bar_directory/bars.rb
@@ -0,0 +1,9 @@
+class ManhattanBarDirectory::Bars
+    attr_accessor :name, :description
+    @@all = []
+
+    def self.new_from_page(bar)
+
+    end
+
+end
--- manhattan_bar_directory/lib/manhattan_bar_directory/cli.rb
+++ manhattan_bar_directory/lib/manhattan_bar_directory/cli.rb
@@ -3,6 +3,7 @@ class ManhattanBarDirectory::CLI
   def call
     list_neighborhoods
     menu
+    goodbye
   end
 
   def list_neighborhoods
@@ -29,7 +30,74 @@ class ManhattanBarDirectory::CLI
   end
 
   def menu
-    puts "Enter the number of the neighborhood where you would like to drink."
+    input = nil
+    while input != "exit"
+      puts "Please enter the number of the neighborhood where you would like to drink, or type list to see the options again, or type exit to leave."
+      input = gets.strip
+      case input
+      when "1"
+        puts "These are some great bars in the East Village:"
+      when "2"
+        puts "These are some great bars in the West Village:"
+      when "3"
+        puts "These are some great bars in Greenwich Village:"
+      when "4"
+        puts "These are some great bars in Soho & Nolita:"
+      when "5"
+        puts "These are some great bars in the Lower East Side & Chinatown:"
+      when "6"
+        puts "These are some great bars in Tribeca:"
+      when "7"
+        puts "These are some great bars in the Financial District:"
+      when "8"
+        puts "These are some great bars in Flatiron/ Nomad:"
+      when "9"
+        puts "These are some great bars in Gramercy/ Murray Hill:"
+      when "10"
+        puts "These are some great bars in Chelsea:"
+      when "11"
+        puts "These are some great bars in Midtown East:"
+      when "12"
+        puts "These are some great bars in Midtown West:"
+      when "13"
+        puts "These are some great bars in the Upper East Side:"
+      when "14"
+        puts "These are some great bars in the Upper West Side:"
+      when "15"
+        puts "These are some great bars in Harlem, East Harlem, & Morningside Heights"
+      when "16"
+        puts "These are some great bars in Washington Heights/ Inwood:"
+      when "list"
+        list_neighborhoods
+      else
+        puts "Have you been drinking already? That's not one of our options! Type list or exit."
+      end
+    end
+  end
+
+  def goodbye
+    puts "Have fun and drink responsibly!"
+  end
+
+  def print_bars(from_neighborhood)
+    puts ""
+    puts "---------- Bars in #{from_neighborhood} ----------"
+    puts ""
+    ManhattanBarDirectory::Bars.all[from_neighborhood-1].each.with_index(from_neighborhood) do |bar, index|
+      puts "#{index}. #{bar.name}"
+    end
+  end
+
+  def print_bar(bar)
+    puts ""
+puts "----------- #{bar.name} -----------"
+puts ""
+puts "Location:           #{restaurant.location}"
+puts ""
+puts "---------------Description--------------"
+puts ""
+puts "#{restaurant.description}"
+puts ""
   end
 
 end
